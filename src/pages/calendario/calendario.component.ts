69fb635a3b1a881a89862b2ec59732053a92f542
LuisRQM <luisalberto20304@hotmail.com>
2025-08-16 00:40:40 -0600

    add views users and register

--- src/pages/calendario/calendario.component.ts
+++ src/pages/calendario/calendario.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-calendario',
   templateUrl: './calendario.component.html',
-  styleUrls: ['./calendario.component.css']
+  styleUrls: ['./calendario.component.css'],
+  imports: [
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatDialogModule,
+    CommonModule,
+    MatCardModule,
+    MatSelectModule
+   
+  ],
+  standalone: true
 })
 export class CalendarioComponent { }
--- src/pages/usuarios/usuarios.component.ts
+++ src/pages/usuarios/usuarios.component.ts
@@ -9,6 +9,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { RegistrarUsuarioDialogComponent } from './registrar-usuario-dialog/registrar-usuario-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
+
 
 export interface Usuario {
   nombre: string;
@@ -17,11 +23,18 @@ export interface Usuario {
   rol: string;
   club: string;
   categoria: string;
+  fotoPerfil: string;
+  identificacion: string;
+  genero: string;
+  correo: string;
+  apellidos: string;
+  manoPreferida: string;
 }
 
 const USUARIOS_DATA: Usuario[] = [
-  { nombre: 'Luis Ramirez', victorias: '5/11', puntos: 124, rol: 'Jugador', club: 'MexPadel', categoria: 'Varonil' },
-  // ... más usuarios
+  { nombre: 'Luis', victorias: '5/11', puntos: 124, rol: 'Jugador', club: 'MexPadel', categoria: 'Varonil', fotoPerfil: '../../assets/images/userm.jpg', identificacion: '../../assets/images/ine.png', genero: 'Masculino', correo: 'luisalberto20304@hotmail.com', apellidos: 'Ramirez', manoPreferida: 'Derecha' },
+  { nombre: 'Ramona', victorias: '5/11', puntos: 258, rol: 'Jugador', club: 'MexPadel', categoria: 'Femenino', fotoPerfil: '../../assets/images/userm.jpg', identificacion: '../../assets/images/ine.png', genero: 'Masculino', correo: 'ramona20304@hotmail.com', apellidos: 'Lopez', manoPreferida: 'Izquierda' },
+
 ];
 
 @Component({
@@ -31,22 +44,26 @@ const USUARIOS_DATA: Usuario[] = [
   standalone: true,
   imports: [
     CommonModule,
-    MatTableModule,      
-    MatPaginatorModule,   
+    MatTableModule,
+    MatPaginatorModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
     MatExpansionModule,
     MatDialogModule,
-    MatCardModule
+    MatCardModule,
+    MatDividerModule,
+    FormsModule,
+    MatSelectModule
   ]
 })
 export class UsuariosComponent implements OnInit {
 
   displayedColumns: string[] = ['nombre', 'victorias', 'puntos', 'rol', 'club', 'categoria', 'acciones'];
   dataSource = new MatTableDataSource<Usuario>(USUARIOS_DATA);
-
+  editando: boolean = false;
+  formUsuario: any = {};
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   ngOnInit() {
@@ -61,4 +78,44 @@ export class UsuariosComponent implements OnInit {
   eliminarUsuario(usuario: Usuario) {
     console.log('Eliminar usuario', usuario);
   }
+
+  selectedUsuario: any = null;
+
+  verDetalle(usuario: any) {
+    this.selectedUsuario = usuario;
+  }
+
+  volverLista() {
+    this.selectedUsuario = null;
+  }
+
+  editarUsuario(usuario: any) {
+  this.editando = true;
+  this.formUsuario = { ...usuario }; // copia de datos para edición
+}
+
+guardarCambios() {
+  // Aquí llamas a tu servicio para actualizar
+  this.selectedUsuario = { ...this.formUsuario };
+  this.editando = false;
+}
+
+cancelarEdicion() {
+  this.editando = false;
+  this.formUsuario = { ...this.selectedUsuario }; // restaurar datos originales
+}
+
+
+constructor(private dialog: MatDialog) { }
+
+  abrirModalRegistrarUsuario() {
+    this.dialog.open(RegistrarUsuarioDialogComponent, {
+      width: '800px',
+      maxWidth: '50vw',
+      height: 'auto',
+      maxHeight: '70vh',
+      panelClass: 'custom-dialog'
+    });
+}
+
 }
