2e236b281932fe8fb89a7ee1ff173cb08017e827
Humberto <betohg0416@gmail.com>
2025-08-28 00:25:39 -0600

    Horarios en canchas

--- src/app/services/horarios.service.ts
+++ src/app/services/horarios.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { ApiBaseService } from './api.service';
 
 export interface Horario {
   id?: number;
@@ -14,29 +14,25 @@ export interface Horario {
 @Injectable({
   providedIn: 'root'
 })
-export class HorariosService {
-
-  private apiUrl = 'http://127.0.0.1:8000/api/clubShedule';
-
-  constructor(private http: HttpClient) { }
+export class HorariosService extends ApiBaseService {
 
   getHorarios(): Observable<Horario[]> {
-    return this.http.get<Horario[]>(`${this.apiUrl}`);
+    return this.get<Horario[]>('/horarios');
   }
 
   getHorario(id: number): Observable<Horario> {
-    return this.http.get<Horario>(`${this.apiUrl}/${id}`);
+    return this.get<Horario>(`/horarios/${id}`);
   }
 
-  createHorario(horario: Horario): Observable<Horario> {
-    return this.http.post<Horario>(`${this.apiUrl}/create`, horario);
+  createHorario(data: Partial<Horario>): Observable<any> {
+    return this.post('/courtSchedules/create', data);
   }
 
-  updateHorario(id: number, horario: Horario): Observable<Horario> {
-    return this.http.put<Horario>(`${this.apiUrl}/update/${id}`, horario);
+  updateHorario(id: number, data: Partial<Horario>): Observable<any> {
+    return this.put(`/horarios/update/${id}`, data);
   }
 
-  deleteHorario(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/delete/${id}`);
+  deleteHorario(id: number): Observable<any> {
+    return this.delete(`/horarios/delete/${id}`);
   }
 }
